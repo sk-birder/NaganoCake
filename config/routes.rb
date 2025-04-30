5fafcb77f924b0ca641b53a8fe103f9312c94053
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-04-30 06:38:27 +0000

    [Adjust] Devise Controllerの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # 顧客側のURL変更
+  devise_for :customers, skip: [:password], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  # 管理者側のURL変更
+  devise_for :admins, skip: [:password, :registrations], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
