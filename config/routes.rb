0b2515629b4f3c3ddede197bdfce314c2dc4b513
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-05-04 02:07:54 +0000

    [Edit] Routing編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,60 @@
 Rails.application.routes.draw do
+  # public/homes ルートディレクトリの設定を兼ねる
   root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+
+  # public/items
+  resources :items, only: [:index, :show], controller: 'public/items'
+
+  # public/customers
+  # resourcesではなくresourceを使う(:idを扱わないため)
+  namespace :customers do
+    resource :my_page, only: [:show, :edit, :update], controller: '/public/customers'
+    get 'my_page/confirm' => '/public/customers#confirm'
+    patch 'my_page' => '/public/customers#withdraw'
+  end
+
+  # cart_items
+  # get 'cart_items/index' ここから作業
+
 
   namespace :admin do
     get 'orders/show'
-  end
-  namespace :admin do
+  
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
-  end
-  namespace :admin do
+  
     get 'genres/index'
     get 'genres/edit'
-  end
-  namespace :admin do
+  
     get 'items/index'
     get 'items/new'
     get 'items/show'
     get 'items/edit'
-  end
-  namespace :admin do
+  
     get 'homes/top'
   end
+
   namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
+    # homes
+    # 最上部に記述
+
+
+
+
+    # orders
     get 'orders/new'
     get 'orders/confirm'
     get 'orders/complete'
     get 'orders/index'
     get 'orders/show'
+
+    # addresses
+    get 'addresses/index'
+    get 'addresses/edit'
   end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/confirm'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
   # 顧客側のURL変更
   devise_for :customers, skip: [:password], controllers: {
     registrations: "public/registrations",
