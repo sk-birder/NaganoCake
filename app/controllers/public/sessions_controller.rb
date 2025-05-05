e917acb024958a03483cccd72b587f572c3029ce
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-05-05 15:17:28 +0000

    [Add] サインイン機能に退会済みか判定する機能を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  # is_active判定用メソッドの呼び出し createであることに注意
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -33,4 +35,16 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+  def customer_state
+    # 入力されたemailがCustomerモデルに存在するか判定
+    customer = Customer.find_by(email: params[:customer][:email])
+    # 存在しなければreturn
+    return if customer.nil?
+    # 存在すればパスワードの照合 不一致ならばreturn
+    return unless customer.valid_password?(params[:customer][:password])
+    # パスワード一致てもis_activeがfalseならばサインアップ画面へ遷移
+    redirect_to new_customer_registration_path unless customer.is_active
+  end
 end
