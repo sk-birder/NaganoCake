ea59ac00b82aea1ceca7f90c8bcafaff8ef65856
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-05-08 12:27:14 +0000

    [Add] ジャンル機能の追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,11 +3,27 @@ class Admin::GenresController < ApplicationController
 
   def index
     @new_genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @new_genre = Genre.new(genre_params)
+    @new_genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
+    @edit_genre = Genre.find(params[:id])
+  end
+
+  def update
+    @edit_genre = Genre.find(params[:id])
+    @edit_genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
